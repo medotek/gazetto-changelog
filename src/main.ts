03c7c0b131efd67852492c57eee149b06ede0a03
Thomas Huynh <thuynh@axome.com>
2023-04-25 18:43:40 +0200

    [+] Init project

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import BootstrapVueNext from 'bootstrap-vue-next'
+
+import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+const app = createApp(App)
+app.use(BootstrapVueNext)
+app.mount('#app')
